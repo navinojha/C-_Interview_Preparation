de9ef4982c2cc4be81a4995cadb247c0d33fa164
Navin Ojha <navinojha1996@gmail.com>
2022-03-29 00:57:14 +0530

    Approach-1 Rotate an array by k units

---
+++ Arrays/rotate_array.cpp
@@ -0,0 +1,51 @@
+// Rotate an array clockwise by K units
+
+// Example : [1, 3, 5, 7, 9] k = 2
+// Output : [7, 9, 1, 3, 5]
+
+// Approach -1 (Bruteforce Approach Time Complexity - (O(n^2)))
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int a[n];
+
+  for(int i=0;i<n;i++)
+  {
+    cin >> a[i];
+  }
+
+  int k;
+  cin >> k;
+
+  while(k--)
+  {
+    int temp = a[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+      a[i+1] = a[i];
+    }
+    a[0] = temp;
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    cout << a[i] << " ";
+  }
+  cout << "\n";
+  return 0;
+}
+
+// Approach - 2 (Can we use some extra space to rotate the array)
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+
+
+
+
