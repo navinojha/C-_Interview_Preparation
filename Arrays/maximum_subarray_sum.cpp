b99b41f42f5daae7dd94625094ad8ddd5b527f82
Navin Ojha <navinojha1996@gmail.com>
2022-03-28 12:44:17 +0530

    Optimized using Prefix Sum Method

--- Arrays/maximum_subarray_sum.cpp
+++ Arrays/maximum_subarray_sum.cpp
@@ -10,34 +10,76 @@
 
 // Approach -1 (O(n^3) Approach)
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+
+//   int a[n];
+//   for(int i=0;i<n;i++)
+//   {
+//     cin >> a[i];
+//   }  
+//   int sum = 0;
+//   int maxSum = INT_MIN;
+//   for(int i=0;i<n;i++)
+//   {
+//     for(int j=i+1;j<n;j++)
+//     {
+//       sum = 0;
+//       for(int k=i ;k <= j;k++)
+//       {
+//         sum = sum + a[k];
+//       }
+//       maxSum = max(sum, maxSum);
+//     }
+//   }
+
+//   cout << maxSum << "\n";
+//   return 0;
+// }
+
+// Approach -2 (We are using some kind of pre-computation technique i.e prefix Sum)
+
+// Time complexity : O(n^2) Space Complexity : O(n)
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
   int n;
   cin >> n;
-
+   
   int a[n];
+
   for(int i=0;i<n;i++)
   {
     cin >> a[i];
-  }  
-  int sum = 0;
-  int maxSum = INT_MIN;
+  }
+   
+  int prefixSum[n] = {0};
+  prefixSum[0] = a[0];
+
+  for(int i=1;i<n;i++)
+  {
+    prefixSum[i] = prefixSum[i-1] + a[i]; // calculating the prefix sum
+  }
+  
+  int maxSubarraySum = INT_MIN;
   for(int i=0;i<n;i++)
   {
-    for(int j=i+1;j<n;j++)
+    for(int j=i;j<n;j++)
     {
-      sum = 0;
-      for(int k=i ;k <= j;k++)
-      {
-        sum = sum + a[k];
-      }
-      maxSum = max(sum, maxSum);
+      int currentSubarraySum = i > 0 ? (prefixSum[j] - prefixSum[i-1]) : prefixSum[j]; // checking for index out of bound
+      maxSubarraySum = max(maxSubarraySum, currentSubarraySum); // updating the subarray sum
     }
   }
 
-  cout << maxSum << "\n";
+  cout << maxSubarraySum << "\n";
   return 0;
+
 }
