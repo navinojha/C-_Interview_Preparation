71ebbd5c539eeaf189e9c6d608f58bb1c84b7fc8
Navin Ojha <navinojha1996@gmail.com>
2022-03-29 02:30:24 +0530

    Approach -3 using reverse in parts logic

--- Arrays/rotate_array.cpp
+++ Arrays/rotate_array.cpp
@@ -45,8 +45,55 @@
 // Time Complexity : O(n)
 // Space Complexity : O(n)
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//   int n;
+//   cin >> n;
+
+//   int a[n];
+
+//   for(int i=0;i<n;i++)
+//   {
+//     cin >> a[i];
+//   }
+
+//   int k;
+//   cin >> k;
+
+//   int temp[n];
+
+//   for(int i=0;i<n;i++)
+//   {
+//     temp[(i+k)%n] = a[i];
+//   }
+
+//   for(int i=0;i<n;i++)
+//   {
+//     cout << temp[i] << " ";
+//   }
+//   cout << "\n";
+//   return 0;
+// }
+
+// Approach -3 (Using reversal of array logic)
+
+// Time Complexity - O(n) Space Complexity - O(1)
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void reverseArray(int a[], int startIndex, int endIndex)
+{
+   while(startIndex <= endIndex){   
+    swap(a[startIndex], a[endIndex]);
+    startIndex++;
+    endIndex--;
+   }
+}
 
 int main()
 {
@@ -63,20 +110,16 @@ int main()
   int k;
   cin >> k;
 
-  int temp[n];
+  reverseArray(a,0,n-k-1);  // [1, 3, 5, 7, 9]  reverseArray(a, 0, 2)
+  reverseArray(a,k+1,n-1);  // -> [5, 3, 1]  [9, 7] reverseArray(a,3,4);
+  reverseArray(a,0,n-1);   //-> [9, 7, 1, 3 ,5] reverseArray(a, 0, 4);
 
   for(int i=0;i<n;i++)
   {
-    temp[(i+k)%n] = a[i];
-  }
-
-  for(int i=0;i<n;i++)
-  {
-    cout << temp[i] << " ";
+    cout << a[i] << " ";
   }
   cout << "\n";
   return 0;
 }
-
 
 
